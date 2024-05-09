566ccf558e58c6b25840a7d1403034fe349654b6
tonio-brank <iab_blackriver@hotmail.com>
2024-05-09 18:11:45 +0200

    stitching own image + trial video

--- features_matching/image_cutting_unwrapping.py
+++ features_matching/image_cutting_unwrapping.py
@@ -11,25 +11,25 @@ def normalize_coordinate(x, y, width_equirect, height_equirect):
     return (x_prime, y_prime)
 
 def long_lat(x_prime, y_prime, lambda_0):
-    longitude = math.pi * x_prime + lambda_0
-    latitude = math.pi/2 * y_prime
+    longitude = np.pi * x_prime + lambda_0
+    latitude = np.pi/2 * y_prime
     return (longitude, latitude)
 
 
 def normalized_2D_fisheye_coord(latitude, longitude, aperture_in_rad, width, height, x, y):
-    P_x = math.cos(latitude) * math.cos(longitude)
-    P_y = math.cos(latitude) * math.sin(longitude)
-    P_z = math.sin(latitude)
+    P_x = np.cos(latitude) * np.cos(longitude)
+    P_y = np.cos(latitude) * np.sin(longitude)
+    P_z = np.sin(latitude)
 
-    theta = math.atan2(P_z, P_x)
+    theta = np.arctan2(P_z, P_x)
 
-    sqrt_for_r = math.sqrt(P_x ** 2 + P_z ** 2)
+    sqrt_for_r = np.sqrt(P_x ** 2 + P_z ** 2)
     if (P_y != 0):
-        r = 2 * math.atan2(sqrt_for_r, P_y) / aperture_in_rad
+        r = 2 * np.arctan2(sqrt_for_r, P_y) / aperture_in_rad
     else:
         r = 0
 
-    x_2 = (width - 1) * (theta / (2 * math.pi) + 0.5)
+    x_2 = (width - 1) * (theta / (2 * np.pi) + 0.5)
     y_2 = (height - 1) * (r / 2)
 
     return (x_2, y_2)
@@ -37,20 +37,20 @@ def normalized_2D_fisheye_coord(latitude, longitude, aperture_in_rad, width, hei
 
 def compute_polar_angles(x_dest, y_dest, width_equirect, height_equirect):
     # -pi to pi
-    theta = 2.0 * math.pi * (x_dest / width_equirect - 0.5)
+    theta = 2.0 * np.pi * (x_dest / width_equirect - 0.5)
     # -pi/2 to pi/2
-    phi = math.pi * (y_dest / height_equirect - 0.5)
+    phi = np.pi * (y_dest / height_equirect - 0.5)
     return (theta, phi)
 
 def vector_in_3D_space(theta, phi):
-    x = math.cos(phi) * math.sin(theta)
-    y = math.cos(phi) * math.cos(theta)
-    z = math.sin(phi)
+    x = np.cos(phi) * np.sin(theta)
+    y = np.cos(phi) * np.cos(theta)
+    z = np.sin(phi)
     return (x, y, z)
 
 def calculate_fisheye_angle_radius(x, y, z, width, fov):
-    theta = math.atan2(z, x)
-    phi = math.atan2(math.sqrt(x**2 + z ** 2), y)
+    theta = np.arctan2(z, x)
+    phi = np.arctan2(np.sqrt(x**2 + z ** 2), y)
     r = width * phi / fov
     return (theta, phi, r)
 
@@ -61,11 +61,29 @@ def fish2sphere(x, y, width, height, fov, width_equirect, height_equirect):
     (theta_sphere, phi_sphere, r_sphere) = calculate_fisheye_angle_radius(x_3d, y_3d, z_3d, width, fov)
 
     # fisheye space
-    x_pfish = 0.5 * width + r_sphere * math.cos(theta_sphere)
-    y_pfish = 0.5 * width + r_sphere * math.sin(theta_sphere)
+    x_pfish = 0.5 * width + r_sphere * np.cos(theta_sphere)
+    y_pfish = 0.5 * width + r_sphere * np.sin(theta_sphere)
 
     return (x_pfish, y_pfish)
 
+def crop_non_zero_region(image):
+    # Find coordinates of non-zero pixels
+    non_zero_coords = np.argwhere(np.any(image != 0, axis=-1))
+    
+    # Extract x and y coordinates
+    x_coords = non_zero_coords[:, 1]
+    y_coords = non_zero_coords[:, 0]
+    
+    # Find bounding box coordinates
+    min_x = np.min(x_coords)
+    max_x = np.max(x_coords)
+    min_y = np.min(y_coords)
+    max_y = np.max(y_coords)
+    
+    # Crop the image using the bounding box coordinates
+    cropped_image = image[min_y:max_y+1, min_x:max_x+1]
+    
+    return cropped_image
 
 def project_image(input_image_path, output_image_path, fov):
     fisheye_image = cv2.imread(input_image_path)
@@ -75,6 +93,42 @@ def project_image(input_image_path, output_image_path, fov):
     height_equirect = height
 
     aperture = math.radians(fov)
+
+    # Create a blank image for the unwrapped equirectangular projection
+    unwrapped_image = np.zeros((height_equirect, width_equirect, 3), dtype=np.uint8)
+
+    # Generate meshgrid of coordinates for the unwrapped image
+    x_indices = np.arange(width_equirect)
+    y_indices = np.arange(height_equirect)
+    xx, yy = np.meshgrid(x_indices, y_indices)
+
+    # Convert meshgrid coordinates to fish-eye coordinates
+    x_pfish, y_pfish = fish2sphere(xx, yy, width, height, aperture, width_equirect, height_equirect)
+    # print(x_pfish.shape)
+
+    # Clip coordinates to ensure they fall within the bounds of the fisheye image
+    x_pfish_clipped = np.clip(x_pfish, 0, width - 1)
+    y_pfish_clipped = np.clip(y_pfish, 0, height - 1)
+
+    # Map the pixel values from fisheye image to the unwrapped image
+    unwrapped_image = fisheye_image[y_pfish_clipped.astype(int), x_pfish_clipped.astype(int)][:, width_equirect//4:3*width_equirect//4, :]
+    
+    # no_info_coords = np.argwhere(np.all(unwrapped_image == 0, axis=-1))
+    # unwrapped_image =  crop_non_zero_region(unwrapped_image)
+    # print(no_info_coords.shape)
+
+    # Save the unwrapped image
+    cv2.imwrite(output_image_path, unwrapped_image)
+    return unwrapped_image
+
+def project_image_loop(input_image_path, output_image_path, fov):
+    fisheye_image = cv2.imread(input_image_path)
+    height, width = fisheye_image.shape[:2]
+
+    width_equirect = width * 2
+    height_equirect = height
+
+    aperture = np.radians(fov)
     l = []
 
     # Create a blank image for the unwrapped equirectangular projection
@@ -94,9 +148,8 @@ def project_image(input_image_path, output_image_path, fov):
     full_image = np.argwhere(taken_points == height_equirect)
     start = full_image[0][0] - 5
     end = full_image[-1][0] + 5
-    print(start, end)
-    # print(np.max())
-    unwrapped_image = unwrapped_image[:, start:end, :]
+    
+    unwrapped_image = unwrapped_image[:, start+50:end-50, :]
     
     
     
@@ -109,10 +162,20 @@ def project_image(input_image_path, output_image_path, fov):
 def split_image(input_image_path, output_image_path1, output_image_path2):
     # Open the image
     image = Image.open(input_image_path)
+    print(f"new size: {image.size}")
+
 
     # Get the width and height of the image
     width, height = image.size
 
+# Calculate new dimensions by dividing by 2
+
+
+# Use the resize method to downsample the image
+    # image = image.resize((width//2, height//2 ), Image.ANTIALIAS)
+    # width, height = image.size
+
+    # print(f"new size: {image.size}")
     # Calculate the midpoint
     midpoint = width // 2
 
--- features_matching/test_orb.py
+++ features_matching/test_orb.py
@@ -34,237 +34,222 @@ def imageWithKeypoints(file):
                                  flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS) 
     return output_image
 
-f1 = "left.jpg"
-f2 = "right.jpg"
-# f1 = "./imgs/unwrapped_walgreens_left.jpg"
-# f2 = "./imgs/unwrapped_walgreens_right.jpg"
-# f1 = "part1.jpg"
-# f2 = "part2.jpg"
-# f1 = "unrelated1.jpg"
-# f2 = "unrelated2.jpg"
 
-width = 150
+def stitch(file_name, debug=False):
 
-img1_full, img2_full = unwrap_and_cut_img("./test.jpg", f1, f2, 190)
-img1 = img1_full
-img2 = img2_full
-# img1 = img1_full[:, 0:width, :]
-# img2 = img2_full[:, -width:, :]
-
-# fig, axs = plt.subplots(1, 2)
-
-# axs[0].imshow(img1[:,:,::-1])
-# axs[0].axis(False)
-# axs[1].imshow(img2[:,:,::-1])
-# axs[1].axis(False)
-
-# plt.tight_layout()
-# plt.show()
+    f1 = "left.jpg"
+    f2 = "right.jpg"
+    # f1 = "./imgs/unwrapped_walgreens_left.jpg"
+    # f2 = "./imgs/unwrapped_walgreens_right.jpg"
+    # f1 = "part1.jpg"
+    # f2 = "part2.jpg"
+    # f1 = "unrelated1.jpg"
+    # f2 = "unrelated2.jpg"
 
 
-# img1 = cv.cvtColor(img1, cv.COLOR_BGR2GRAY) 
-# img2 = cv.cvtColor(img2, cv.COLOR_BGR2GRAY) 
-# exit()
-# img1 = openImgGray(f1)
-# img2 = openImgGray(f2)
-# img1 = rgb2gray(img1.T)
-# img2 = rgb2gray(img2)
-# plt.imshow(img1, cmap="gray")
-# plt.show()
+    img1_full, img2_full = unwrap_and_cut_img(f"../frames_outdoor/{file_name}", f1, f2, 190) # here img1 are open cv images so coordinates are reversed
+    # h, w = img1_full.shape
+    width = int(img1_full.shape[1]*(190-185)/100)
+    # h, w = img1_full.size
+    # img1 = img1_full
+    # img2 = img2_full
+    img1 = img1_full[:, 0:width, :]
+    img2 = img2_full[:, -width:, :]
+    
+    if debug:
+        fig, axs = plt.subplots(1, 2)
 
+        axs[0].imshow(img1[:,:,::-1])
+        axs[0].axis(False)
+        axs[1].imshow(img2[:,:,::-1])
+        axs[1].axis(False)
 
-# exit(0)
-
-kp1, des1 = computeDescriptors(f1, use_img=False)
-kp2, des2 = computeDescriptors(f2, use_img=False)
-# kp1, des1 = computeDescriptors(img1, use_img=True)
-# kp2, des2 = computeDescriptors(img2, use_img=True)
-
-bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
- 
-# Match descriptors.
-matches = bf.match(des1,des2)
- 
-# Sort them in the order of their distance.
-matches = sorted(matches, key = lambda x:x.distance)
-# print(matches[10].distance)
- 
-n_match = 20
-matches = matches[:n_match]
-edge_matches = []
-print("dimension 1")
-print(img1.shape)
-print("dimension 2")
-print(img2.shape)
-
-#TODO change as percentage
-thresh = 100
-
-src_array = []
-dst_array = []
+        plt.tight_layout()
+        plt.show()
 
 
 
 
-for match in matches:
-    img1_idx = match.queryIdx
-    img2_idx = match.trainIdx
-    print("distance")
-    print(match.distance)
 
-    # x - columns
-    # y - rows
-    # Get the coordinates
-    (x1, y1) = kp1[img1_idx].pt
-    (x2, y2) = kp2[img2_idx].pt
+    kp1, des1 = computeDescriptors(img1, use_img=True)
+    kp2, des2 = computeDescriptors(img2, use_img=True)
+    # kp1, des1 = computeDescriptors(img1, use_img=True)
+    # kp2, des2 = computeDescriptors(img2, use_img=True)
 
-    if ((x1 < thresh and x2 > img2.shape[0] - thresh)):
+    bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
+    
+    # Match descriptors.
+    matches = bf.match(des1,des2)
+    
+    # Sort them in the order of their distance.
+    matches = sorted(matches, key = lambda x:x.distance)
+    # print(matches[10].distance)
+    
+    n_match = 20
+    matches = matches[:n_match]
+    edge_matches = []
+    print("dimension 1")
+    print(img1.shape)
+    print("dimension 2")
+    print(img2.shape)
+
+    #TODO change as percentage
+    thresh = 150
+
+    src_array = []
+    dst_array = []
+
+
+
+
+    for match in matches:
+        img1_idx = match.queryIdx
+        img2_idx = match.trainIdx
         # print("distance")
-    # if x1 > img1.shape[0] - thresh and x2 < thresh :
+        # print(match.distance)
+
+        # x - columns
+        # y - rows
+        # Get the coordinates
+        (x1, y1) = kp1[img1_idx].pt
+        (x2, y2) = kp2[img2_idx].pt
+
+        # if ((x1 < thresh and x2 > img2.shape[0] - thresh)):
+            # print("distance")
+        # if x1 > img1.shape[0] - thresh and x2 < thresh :
         # print("matched ", match.distance)
-        # print("match")
-        # print(x1, y1)
-        # print(x2, y2)
-    # if match.distance < 25:
-        edge_matches.append(match)
-        src_array.append((x1, y1))
-        dst_array.append((x2, y2))
-    
-# print(edge_matches)
+            # print("match")
+            # print(x1, y1)
+            # print(x2, y2)
+        # if match.distance < 35: #TODO check rule for distance
+        if abs(y1- y2) <20: # we restrict features not aligned horizontally
+            edge_matches.append(match)
+            src_array.append((x1, y1))
+            dst_array.append((x2, y2))
 
-src_array = np.array(src_array)
-dst_array = np.array(dst_array)
+    # print(edge_matches)
 
-print(src_array)
-print(dst_array)
+    src_array = np.array(src_array)
+    dst_array = np.array(dst_array)
 
-   
-
-
-src_pts = np.float32([ kp1[m.queryIdx].pt for m in edge_matches ]).reshape(-1,1,2)
-dst_pts = np.float32([ kp2[m.trainIdx].pt for m in edge_matches ]).reshape(-1,1,2)
-
-
-# M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC)
-M_skimage = transform.estimate_transform('euclidean', src_array, dst_array)
-print(M_skimage)
-tr_matrix = M_skimage.params
-tr_matrix[0, 0] = 1
-tr_matrix[0, 1] = 0
-tr_matrix[1, 0] = 0
-tr_matrix[1, 1] = 1
-tr_matrix[1, 2] = 1
-# tr_matrix[0, 2] = (img1_full.shape[1] - width + 1.5*tr_matrix[0, 2])
-
-overlapping_width = int(np.ceil(img1.shape[1] - M_skimage.params[0, 2]))
-print("overlapping width: ", overlapping_width)
-
-
-src_array = np.hstack((src_array, np.ones((src_array.shape[0], 1)))).T
-dst_array = np.hstack((dst_array, np.ones((dst_array.shape[0], 1)))).T
-
-# dst_comp = M_skimage.params@src_array
-# print("diff")
-# print(dst_comp - dst_array)
-
-# M, mask = cv.findHomography(list_kp1, list_kp2, cv.RANSAC,5.0)
-
-shape = [2*img2_full.shape[0], 2*img2_full.shape[1], 3]
-final_img = np.zeros(shape)
-final_img[0:img2_full.shape[0], 0:img2_full.shape[1], :] = img2_full
-
-
-
-img_mod = np.zeros_like(img2_full)
-
-img1_full = img1_full.astype(np.float32)
-for i in range(overlapping_width):
-    img1_full[:, i, :]*=(i/overlapping_width)
-img1_full = img1_full.astype(np.uint8)
-img_mod = cv.warpPerspective(img1_full, tr_matrix, [2*img2_full.shape[1], 2*img2_full.shape[0]] )
-
+    print(src_array)
+    print(dst_array)
 
     
-plt.imshow(img_mod)
-plt.show()
-# img_mod = transform.warp(img2, M_skimage.inverse)
 
 
-# fig, axs = plt.subplots(1, 2)
-
-# print(final_img)
-
-final_shape = final_img.shape
-
-nonzero_mask = (img_mod != 0) & (final_img !=0)
-
-# img_mod_mask = (img_mod != 0)[:, :, 0].astype(np.uint8)
-
-# # r = nonzero_mask[:, :, 0].astype(np.uint8)
-# plt.imshow(img_mod_mask)
-# plt.show()
-# print(np.argwhere(r ==1))
-final_img += img_mod
-# final_img[nonzero_mask] /= 2
-final_img = final_img.astype(np.float64)
-for i in range(overlapping_width):
-    final_img[:, int(tr_matrix[0, 2])+ i, :] /= (1 + i/overlapping_width)
-final_img = final_img.astype(np.uint8)
-
-final_img[:, 0:overlapping_width, :] =0
+    src_pts = np.float32([ kp1[m.queryIdx].pt for m in edge_matches ]).reshape(-1,1,2)
+    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in edge_matches ]).reshape(-1,1,2)
 
 
-x, y, z = np.nonzero(final_img)
-xl,xr = x.min(),x.max()
-yl,yr = y.min(),y.max()
+    # M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC)
+    M_skimage = transform.estimate_transform('euclidean', src_array, dst_array)
+    print(M_skimage)
+    tr_matrix = M_skimage.params
+    overlapping_width = int(width - tr_matrix[0, 2]) + 1
+    tr_matrix[0, 0] = 1
+    tr_matrix[0, 1] = 0
+    tr_matrix[1, 0] = 0
+    tr_matrix[1, 1] = 1
+    tr_matrix[1, 2] = 1
+    tr_matrix[0, 2] = (img1_full.shape[1] - width + abs(tr_matrix[0, 2]))
+
+    # overlapping_width = int(np.ceil(img1.shape[1] - M_skimage.params[0, 2]))
+    print("overlapping width: ", overlapping_width)
 
 
-final_img = final_img[xl:xr+1, yl:yr+1, :]
-print(final_img.shape)
+    src_array = np.hstack((src_array, np.ones((src_array.shape[0], 1)))).T
+    dst_array = np.hstack((dst_array, np.ones((dst_array.shape[0], 1)))).T
 
-rgb = np.array(final_img)
-rgb[:, :, 0] = final_img[:, :, 2]
-rgb[:, :, 2] = final_img[:, :, 0]
+    # dst_comp = M_skimage.params@src_array
+    # print("diff")
+    # print(dst_comp - dst_array)
 
-rgb = rgb.astype(np.uint8)
+    # M, mask = cv.findHomography(list_kp1, list_kp2, cv.RANSAC,5.0)
 
-im.fromarray(rgb).save("./test/stitched.jpg")
-
-# plt.imshow(final_img, cmap="gray")
-# plt.show()
-# exit()
-# axs[0].imshow(final_img, cmap="gray")  
-# axs[1].imshow(img_mod, cmap="gray")  
-# axs[2].imshow(nonzero_mask, cmap="gray")  
-# plt.show() 
+    shape = [2*img2_full.shape[0], 2*img2_full.shape[1], 3]
+    final_img = np.zeros(shape)
+    final_img[0:img2_full.shape[0], 0:img2_full.shape[1], :] = img2_full
 
 
-# for i in range(n_match):
-#     trainIdx = matches[i].trainIdx
-#     print(kp1[trainIdx].pt)
-# # # # Draw first 10 matches.
-img3 = cv.drawMatches(img1, kp1,img2,kp2,edge_matches,None, matchesThickness=2, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
-# plt.axis(False)
-# plt.tight_layout()
+    img_mod = np.zeros_like(img2_full)
 
-# plt.imshow(img1, cmap="gray")
-# plt.show()
-# plt.savefig("not_matching.jpg", dpi=300)
+    img1_full = img1_full.astype(np.float32)
+    for i in range(overlapping_width):
+        img1_full[:, i, :]*=(i/overlapping_width)
+    img1_full = img1_full.astype(np.uint8)
+    img_mod = cv.warpPerspective(img1_full, tr_matrix, [2*img2_full.shape[1], 2*img2_full.shape[0]] )
 
 
-fig, axs = plt.subplots(1, 2)
-fig.set_size_inches(15, 12)
-# axs[0].imshow(imageWithKeypoints("part1.jpg"))  
-# axs[1].imshow(imageWithKeypoints("part2.jpg"))  
-axs[0].imshow(img3)
-axs[0].axis(False)
-axs[0].set_title("KeyPoints matching between two images")
-axs[1].imshow(rgb)
-axs[1].axis(False)
-axs[1].set_title("Stitched image")
-plt.tight_layout()
-# plt.savefig("test_stiching.jpg", dpi=300)
+    if debug:
+        plt.imshow(img_mod)
+        plt.show()
+    # img_mod = transform.warp(img2, M_skimage.inverse)
 
-plt.show() 
 
+    # fig, axs = plt.subplots(1, 2)
+
+    # print(final_img)
+
+    final_shape = final_img.shape
+
+    nonzero_mask = (img_mod != 0) & (final_img !=0)
+
+    # img_mod_mask = (img_mod != 0)[:, :, 0].astype(np.uint8)
+
+    # # r = nonzero_mask[:, :, 0].astype(np.uint8)
+    # plt.imshow(img_mod_mask)
+    # plt.show()
+    # print(np.argwhere(r ==1))
+    final_img += img_mod
+    # final_img[nonzero_mask] /= 2
+    final_img = final_img.astype(np.float64)
+    for i in range(overlapping_width):
+        final_img[:, int(tr_matrix[0, 2])+ i, :] /= (1 + i/overlapping_width)
+    final_img = final_img.astype(np.uint8)
+
+    # final_img[:, 0:overlapping_width, :] =0
+
+
+    x, y, z = np.nonzero(final_img)
+    xl,xr = x.min(),x.max()
+    yl,yr = y.min(),y.max()
+
+
+    final_img = final_img[xl:xr+1, yl:yr+1, :]
+    print(final_img.shape)
+
+    rgb = np.array(final_img)
+    rgb[:, :, 0] = final_img[:, :, 2]
+    rgb[:, :, 2] = final_img[:, :, 0]
+
+    rgb = rgb.astype(np.uint8)
+
+    im.fromarray(rgb).save(f"../frames_stitched_outdoor/{file_name}")
+
+
+
+
+
+    if debug:
+        img3 = cv.drawMatches(img1, kp1,img2,kp2,edge_matches,None, matchesThickness=2, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+        fig, axs = plt.subplots(1, 2)
+        fig.set_size_inches(15, 12)
+        axs[0].imshow(img3)
+        axs[0].axis(False)
+        axs[0].set_title("KeyPoints matching between two images")
+        axs[1].imshow(rgb)
+        axs[1].axis(False)
+        axs[1].set_title("Stitched image")
+        plt.tight_layout()
+
+        plt.show() 
+
+
+
+for i in range (10, 300):
+    print(f"stitching {i}")
+    try:
+        stitch(f"frame_{i}.jpg")
+    except:pass
